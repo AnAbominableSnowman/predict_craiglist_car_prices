a769c98f4819ff7c650b034d8ce795a2277b1f66
Snowman <devinkestell80@gmail.com>
2024-10-16 15:45:45 -0500

    light gbm works lets process text next

--- lightgbm_approach.py
+++ lightgbm_approach.py
@@ -1,21 +1,8 @@
 import lightgbm as lgb
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import OneHotEncoder
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.metrics import mean_squared_error, r2_score
-import numpy as np
-import statsmodels.api as sm
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
-import matplotlib.pyplot as plt
+from sklearn.metrics import root_mean_squared_error, r2_score
 
 
 cars = pd.read_parquet("output/cleaned_engineered_input.parquet")
@@ -49,7 +36,7 @@ params = {
 
 # # Train the LightGBM model
 model = lgb.train(params, train_data,  num_boost_round=100,valid_sets=[test_data], 
-                  #callbacks=[lgb.early_stopping(stopping_rounds=10)]
+                  callbacks=[lgb.early_stopping(stopping_rounds=10)]
                   )
 
 # Make predictions
@@ -57,15 +44,11 @@ y_pred = model.predict(X_test, num_iteration=model.best_iteration)
 
 
 # Calculate RMSE and R2 score
-rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+rmse = root_mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
 print(f"Root Mean Squared Error (RMSE): {rmse}")
 print(f"R-squared (R2): {r2}")
-
-# Print the summary of the regression model (including beta coefficients, p-values, F-statistic, etc.)
-print("\nModel Summary:")
-# print(model.summary())
 
 # Plot predicted vs actual values
 plt.figure(figsize=(10, 5))
@@ -90,24 +73,3 @@ plt.title("Residuals Plot")
 plt.tight_layout()
 plt.show()
 plt.close()
-# # Convert probabilities to binary outcomes
-# y_pred_binary = [1 if x > 0.5 else 0 for x in y_pred]
-
-# Generate the confusion matrix
-# conf_matrix = confusion_matrix(y_test, y_pred_binary)
-
-# # Display the confusion matrix
-# print("Confusion Matrix:")
-# print(conf_matrix)
-
-# # Plot the confusion matrix
-# plt.figure(figsize=(8, 6))
-# sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=[0, 1], yticklabels=[0, 1])
-# plt.title('Confusion Matrix')
-# plt.xlabel('Predicted Label')
-# plt.ylabel('True Label')
-# plt.show()
-
-# # Optional: Display classification report
-# print("\nClassification Report:")
-# print(classification_report(y_test, y_pred_binary))
