d9fa3355478b575f69ee5f6be178082fb366a33f
Snowman <devinkestell80@gmail.com>
2024-10-21 14:05:48 -0500

    Should i delete out all carvana ads?

--- from step_00_load_and_clean_input import.py
+++ from step_00_load_and_clean_input import.py
@@ -5,24 +5,19 @@ cars = unzip_and_load_csv(r"inputs\vehicles.csv.zip", r"inputs\vehicles_unzipped
 print(cars.columns)
 # Filter rows and select the description
 result = (
-    cars.filter(
-        pl.col("description")
-        .str.to_lowercase()
-        .str.contains("carvana is the safer way to buy a car")
-    )
-    # .limit(10)
-    # .select("description")
-    .height
+    cars.filter(pl.col("description").str.to_lowercase().str.contains("want"))
+    .limit(10)
+    .select("description")
 )
 print(result)
 
-result = (
-    cars.filter(pl.col("description").str.to_lowercase().str.contains("carvana"))
-    # .limit(10)
-    # .select("description")
-    .height
-)
-print(result)
-# # Loop through the result and print each description
-# for description in result["description"]:
-#     print(rf"\n {description}")
+# result = (
+#     cars.filter(pl.col("description").str.to_lowercase().str.contains("carvana"))
+#     # .limit(10)
+#     # .select("description")
+#     .height
+# )
+# print(result)
+# Loop through the result and print each description
+for description in result["description"]:
+    print(rf"\n {description}")
