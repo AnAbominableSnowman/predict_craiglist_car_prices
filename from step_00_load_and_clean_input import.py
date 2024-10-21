dd17a59a8ae4d5413ef8da478664203dda44c73c
Snowman <devinkestell80@gmail.com>
2024-10-21 13:58:59 -0500

    starting paper

--- from step_00_load_and_clean_input import.py
+++ from step_00_load_and_clean_input import.py
@@ -2,7 +2,7 @@ from step_00_load_and_clean_input import unzip_and_load_csv
 import polars as pl
 
 cars = unzip_and_load_csv(r"inputs\vehicles.csv.zip", r"inputs\vehicles_unzipped")
-
+print(cars.columns)
 # Filter rows and select the description
 result = (
     cars.filter(
